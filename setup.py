d9a1457a71bcaaf7dee9522b0a598e148f017626
Matthias Lohr <matthias@lohr.me>
2017-12-14 15:25:06 +0100

    changed version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='domaincheck',
-    version='0.1.0',
+    version='1.0.0',
     author='Matthias Lohr',
     author_email='matthias@lohr.me',
     description='Domain availability check',
